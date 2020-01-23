e260119124308299ee559cf93f6dd75348979546
TACO2112 <45360167+Taco2112@users.noreply.github.com>
2020-01-23 08:32:38 -0500

    Gonna Splash Some Gatorade
    https://youtu.be/SLeje0uLg60 & https://www.youtube.com/watch?v=JS1d7SA-xBg    I like me some orange Gatorade MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏😏

--- Lab11bvst.java
+++ Lab11bvst.java
@@ -1,25 +1,41 @@
 // Lab11bst.java
-// This is the Student starting version of the <Deck> class lab11b assignment. 
+// This is the Student starting version of the <Deck> class lab11b assignment.
 
-
-public class Lab11bst
+//SIMPLE JIST : Display all 52 cards and keep card info has to be in the deck class
+public class Lab11bvst
 {
 	public static void main(String[] args)
-	{ 
+	{
       Deck deck = new Deck();
       System.out.println(deck);
+
+
+
+			/* ACTIVATE WHEN READY or not //helpful
+			System.out.println(deck.size());
+			System.out.println(deck.isEmpty());
+			*/
 	}
 }
 
 
-class Deck 
+class Deck
 {
+	//private: cannot be accesed outside class
 	private Card[] cards;
 	private int size;
+	private String[] suits={"Clubs","Diamonds","Hearts","Spades"};
 
-	public Deck() 
+
+
+	public Deck()
    {
+	//OBJECTIVE:re-define the toString method for the Deck class so that it can be used to display the attribute values in a convenient manner
       size = 52;
 		cards = new Card[size];
-	}    
+		//okay this works Now I have to make it so its like random dued and make it super cool 
+System.out.println(suits[1] + " ");
+
+
+	}
  }
